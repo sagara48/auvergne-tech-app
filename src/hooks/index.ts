613d5d3586104888e0b1c6e47c65e9a0b9b51196
sagara48 <nbres48@gmail.com>
2026-01-31 10:20:08 +0100

    Add files via upload

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,32 @@
 export { useFeuilleHeures } from './useFeuilleHeures';
 export { useRealtimeSubscriptions, useRealtimeTable } from './useRealtimeSubscriptions';
 export { useOnlineStatus, useOfflineCache, getOfflineData } from './useOnlineStatus';
+
+// Hooks centralisés
+export * from './useCentralizedHooks';
+
+// Temps réel
+export {
+  useRealtimeQuery,
+  useRealtimeStatus,
+  useRealtimePannes,
+  useRealtimeTravaux,
+  useRealtimeStock,
+  useRealtimePlanning,
+  useRealtimeCommandes,
+  useRealtimeVehicules,
+  useRealtimeNotes,
+  useRealtimeDocuments,
+  useRealtimeNotifications,
+  useRealtimeAscenseurs,
+  useRealtimeVisites,
+  useRealtimeStats,
+  RealtimeProvider,
+  RealtimeIndicator,
+} from './useRealtime';
+
+// Temps réel global
+export {
+  useGlobalRealtime,
+  RealtimeStatusWidget,
+} from './useGlobalRealtime';
